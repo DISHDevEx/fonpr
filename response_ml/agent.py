96002a518e16d5023391c328dafd3b8b8713d354
Vinayak Sharma <60953073+vinayakSharm@users.noreply.github.com>
2023-03-28 21:10:06 +0000

    adding files to test simple spark streaming in eks

--- response_ml/agent.py
+++ response_ml/agent.py
@@ -1,4 +1,3 @@
-from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 import gzip
 import pandas as pd
@@ -6,7 +5,16 @@ import json
 pd.set_option('display.max_columns', None)
 import numpy as np
 import logging
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
 
+
+
+
+'''
+helper functions
+'''
 
 def gz2df(path):
     with gzip.open(path, 'rb') as f:
@@ -40,56 +48,61 @@ def explode_df(df):
 
 
 
+'''
+actual agent
+'''
 
 if __name__ == "__main__":
     # Create a local StreamingContext with two working thread and batch interval of 15 second
-    sc = SparkContext("local[2]", "responsStream")
-    ssc = StreamingContext(sc, 1)
+    spark = SparkSession.builder.appName("ResponsAgent").getOrCreate()
 
-    # Create a DStream that will connect to hostname:port, like localhost:9999
-    ssc.start() 
-    logging.info("ssc started")
-    ssc.awaitTermination() 
-    lines = ssc.socketTextStream("localhost", 9999)
-    logging.info(lines)
+    # Create DataFrame representing the stream of input lines from connection to localhost:9999    
+    stream = spark.readStream.format("socket").option("host","localhost").option("port", 9999).load()
 
-    df = gz2df(lines)
-    logging.info("data frame created")
-    df['rec_type'] = df.data.apply(get_type)
-    df['data'] = df.data.apply(json.loads)
+    query = stream \
+    .writeStream \
+    .format("console") \
+    .start()
 
-    list_of_df_rectypes = {}  
+    query.awaitTermination()
 
-    for rec_type in df.rec_type.unique():
-        logging.info(f"rec_type: {rec_type}")
-        rec_type_df = filter_by_rec_type(df, rec_type)
-        df_exploded = explode_df(rec_type_df)
-        list_of_df_rectypes[rec_type]=df_exploded
+    # df = gz2df(lines)
+    # logging.info("data frame created")
+    # df['rec_type'] = df.data.apply(get_type)
+    # df['data'] = df.data.apply(json.loads)
 
-    df_pod = list_of_df_rectypes["Pod"]
+    # list_of_df_rectypes = {}  
+
+    # for rec_type in df.rec_type.unique():
+    #     logging.info(f"rec_type: {rec_type}")
+    #     rec_type_df = filter_by_rec_type(df, rec_type)
+    #     df_exploded = explode_df(rec_type_df)
+    #     list_of_df_rectypes[rec_type]=df_exploded
+
+    # df_pod = list_of_df_rectypes["Pod"]
     
-    if("open5gs-upf" in df_pod.PodName.unique()):
-        logging.info("upf data exists")
-        upf_df = df_pod[df_pod["PodName"]=="open5gs-upf" ]
+    # if("open5gs-upf" in df_pod.PodName.unique()):
+    #     logging.info("upf data exists")
+    #     upf_df = df_pod[df_pod["PodName"]=="open5gs-upf" ]
 
-        ## cleaning up the nans in the dataframe
+    #     ## cleaning up the nans in the dataframe
 
-        print(upf_df['pod_cpu_usage_total'].isna().sum())
-        print(len(upf_df))
+    #     print(upf_df['pod_cpu_usage_total'].isna().sum())
+    #     print(len(upf_df))
 
-        upf_df = upf_df.dropna(subset=['pod_cpu_usage_total', 'pod_memory_max_usage' ])
+    #     upf_df = upf_df.dropna(subset=['pod_cpu_usage_total', 'pod_memory_max_usage' ])
 
-        ##setting the limits and requests
-        limit_cpu = max( upf_df[["pod_cpu_usage_total"]].values.tolist() )
-        request_cpu = np.mean( upf_df[["pod_cpu_usage_total"]].values.tolist() ) 
+    #     ##setting the limits and requests
+    #     limit_cpu = max( upf_df[["pod_cpu_usage_total"]].values.tolist() )
+    #     request_cpu = np.mean( upf_df[["pod_cpu_usage_total"]].values.tolist() ) 
 
-        limit_memory =  max( upf_df[["pod_memory_max_usage"]].values.tolist() )
-        request_memory = np.mean( upf_df[["pod_memory_max_usage"]].values.tolist() ) 
+    #     limit_memory =  max( upf_df[["pod_memory_max_usage"]].values.tolist() )
+    #     request_memory = np.mean( upf_df[["pod_memory_max_usage"]].values.tolist() ) 
 
 
-        logging.info(f'limit_cpu: {limit_cpu}')
-        logging.info(f'request_cpu: {request_cpu}')
-        logging.info(f'limit_memory: {limit_memory}')
-        logging.info(f'request_memory: {request_memory}')
+    #     logging.info(f'limit_cpu: {limit_cpu}')
+    #     logging.info(f'request_cpu: {request_cpu}')
+    #     logging.info(f'limit_memory: {limit_memory}')
+    #     logging.info(f'request_memory: {request_memory}')
 
 # ssc.stop()

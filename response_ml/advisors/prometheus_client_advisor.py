13a788a1afa85e1517b14c9e24548d893e857adc
vinnysharm <vinayak.sharma@dish.com>
2023-04-02 19:40:58 +0000

    adding classes for advisors, addint utils

---
+++ response_ml/advisors/prometheus_client_advisor.py
@@ -0,0 +1,28 @@
+from prometheus_api_client import PrometheusConnect
+from datetime import datetime, timedelta
+import pprint
+    
+class PromClient():
+ 
+ def __init__(self, prom_endpoint = 'http://10.0.101.214:9090'):
+  self.prom_endpoint = prom_endpoint
+  self.prom = PrometheusConnect(url=prom_endpoint)
+  self.queries = []
+  self.query_results = []
+  
+ def set_queries(self, query_building_function):
+  self.queries = query_building_function()
+  
+ def get_queries(self):
+  return self.queries 
+ 
+ # max_cpu_query,avg_cpu_query,max_memory_query,avg_memory_query = prom_cpu_mem_queries()
+ 
+ def run_queries(self):
+  ## Aggregate all data:
+  for query in self.queries:
+   self.query_results.append(self.prom.custom_query(query=query))
+  return self.query_results
+
+ 
+ 
--- response_ml/agent.py
+++ response_ml/agent.py
@@ -1,18 +1,25 @@
-# Echo server program
-import socket
-import logging
+from .advisors import prom_client
+from .utilities import prom_cpu_mem_queries
+from collections import defaultdict
+import numpy as np
+import pandas as pd
+#turn the data into a pandas dataframe
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 8888             # Arbitrary non-privileged port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        logging.info('Connected by %r', addr)
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            logging.info(data)
-            print(data)
-            conn.sendall(data)
+dict_lim_req = defaultdict(list)
+prom_client_advisor = prom_client()
+prom_client_advisor.set_queries(prom_cpu_mem_queries)
+max_cpu_data,avg_cpu_data,max_memory_query,avg_memory_query =  prom_client_advisor.run_queries()
+
+
+for i in range(len(max_cpu_data)):
+    try:
+        dict_lim_req[max_cpu_data[i]['metric']['pod']].append(max_cpu_data[i]['value'][1])
+        dict_lim_req[avg_cpu_data[i]['metric']['pod']].append(avg_cpu_data[i]['value'][1])
+        dict_lim_req[max_memory_query[i]['metric']['pod']].append(max_memory_query[i]['value'][1])
+        dict_lim_req[avg_memory_query[i]['metric']['pod']].append(avg_memory_query[i]['value'][1])
+    except KeyError:
+        print("empty records found, ignoring")
+
+df_lim_req = pd.DataFrame.from_dict(dict_lim_req,orient='index', columns = ["lim_cpu_cores","req_cpu_cores","lim_memory_bytes","req_memory_bytes"])
+
+print(df_lim_req.head(100))

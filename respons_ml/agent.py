cc4ef97aebf62ce1937453fadeee229d5aa90385
vinnysharm <vinayak.sharma@dish.com>
2023-04-02 21:56:14 +0000

    agent v0 ready for deployment

--- respons_ml/agent.py
+++ respons_ml/agent.py
@@ -3,6 +3,8 @@ from utilities import prom_cpu_mem_queries
 from collections import defaultdict
 import numpy as np
 import pandas as pd
+import time
+import logging
 
 
 
@@ -12,7 +14,7 @@ def print_lim_reqs():
     prom_client_advisor.set_queries(prom_cpu_mem_queries)
     max_cpu_data,avg_cpu_data,max_memory_data,avg_memory_data =  prom_client_advisor.run_queries()
     dict_lim_req = defaultdict(list)
-    
+    logging.info("making prometheus requests!!")
     for i in range(len(max_cpu_data)):
         try:
             dict_lim_req[max_cpu_data[i]['metric']['pod']].append(max_cpu_data[i]['value'][1])
@@ -24,10 +26,18 @@ def print_lim_reqs():
     
     df_lim_req = pd.DataFrame.from_dict(dict_lim_req,orient='index', columns = ["lim_cpu_cores","req_cpu_cores","lim_memory_bytes","req_memory_bytes"])
     
+    logging.info('\t'+ df_lim_req.to_string().replace('\n', '\n\t')) 
     print(df_lim_req.head(100))
     
     
 if __name__ == "__main__":
-    print("reacher py file")
-    print_lim_reqs()
+    num_iterations = 1,0000
+    time_interval = 5
+    
+    for i in (num_iterations):
+        print_lim_reqs()
+        logging.info("iteration: ", i, "! next update in ", time_interval, " seconds")
+        print("iteration: ", i, "! next update in ", time_interval, " seconds")
+        time.sleep(time_interval)
+        
     

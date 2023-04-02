a3bde19011fe1d1673933a268a17f41a18d76b29
vinnysharm <vinayak.sharma@dish.com>
2023-04-02 05:42:01 +0000

    parsing prom ql queries

--- response_ml/prom.py
+++ response_ml/prom.py
@@ -8,14 +8,14 @@ prom = PrometheusConnect(url=prometheus_url)
 
 ## Queries
 '''how cpu queries works: 
- (1)rate(container_cpu_usage_seconds_total[5m])[3h:]  returns the cpu cores total for 2 minute windows for the last 3 hours
+ (1)rate(container_cpu_usage_seconds_total[2m])[3h:]  returns the cpu cores avg for 2 minute windows for the last 3 hours
     (a) by taking the rate of increase of container_cpu_usage_seconds_total we get the number of CPU-seconds consumed per second.
        (i) container_cpu_usage_seconds_total is a counter (https://prometheus.io/docs/concepts/metric_types/#counter). 1 CPU Second = 1 Core fired up for that second. 
        (ii) rate(m[d])[timewindow] - returns the per-second rate of change for metric for a certain time window
        (iii) rate(x[35s]) = difference in value over 35 seconds / 35s
        (v) rate does not return any results at all if there are less than two samples available.
        (vi) time range should be atleast 2x the scrape interval. So we are using 2m
-       ex/ rate(container_cpu_usage_seconds_total[10m]) is 1.34. This means our container spent an avg of 1.34 CPU seconds per specific seconds. (https://github.com/google/cadvisor/issues/2026)
+       ex/ rate(container_cpu_usage_seconds_total[10m]) is 1.34. This means our container spent an avg of 1.34 CPU seconds per seconds. (https://github.com/google/cadvisor/issues/2026)
     (c)[3h:] returns the last 3 hours of the time series
     
  (2) max_over_time calculates the maximum value over raw samples on the given lookbehind window d per ieach time series returned from the given series_selector. 
@@ -37,5 +37,16 @@ max_memory_query = prom.custom_query(query=max_memory_query)
 avg_memory_query = prom.custom_query(query=avg_memory_query)
 
 # Print the data
-pprint.pprint(max_cpu_data)
+#pprint.pprint(avg_memory_query)
 
+
+#turn the data into a pandas dataframe
+import numpy as np
+import pandas as pd
+
+for i in avg_memory_query:
+ try:
+  print(i['metric']['pod'])
+ except KeyError:
+  print("empty records found, ignoring")
+ 

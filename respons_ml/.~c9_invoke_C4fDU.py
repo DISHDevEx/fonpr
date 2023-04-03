94bc7a305f379246fa8c413bb447e9d7f354d8e6
vinnysharm <vinayak.sharma@dish.com>
2023-04-03 07:37:09 +0000

    removing unnessecary file

--- respons_ml/.~c9_invoke_C4fDU.py
+++
@@ -1,8 +0,0 @@
-"""
-Respons-ML
-A package to react in real time to EKS cluster behavior 
-to fine tune helm charts for deployed applications. 
-"""
-from .utilities import prom_cpu_mem_queries
-
-from .advisors import prom_cpu_mem_queries

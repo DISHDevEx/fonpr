2424a7b6395ed3b9f4f5c453f51fbb97017968dc
matthew.reed <matthew.reed@dish.com>
2023-04-10 18:57:26 +0000

    Updated init files to allow access to ActionHanlder class and get_token function at respons_ml level.

--- respons_ml/action_handler/__init__.py
+++ respons_ml/action_handler/__init__.py
@@ -4,5 +4,4 @@ Automated connection to GitHub through web API to fetch, update, and push
 changes to value.yaml files.
 """
 
-from .action_handler import ActionHandler
-from .action_handler import get_token
+from .action_handler import ActionHandler, get_token

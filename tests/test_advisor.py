d79416a1ac1ca835c649a3fc10ef038324f2f259
vinnysharm <vinayak.sharma@dish.com>
2023-04-10 16:54:34 +0000

    adding pytest pr check yml for github workflows

---
+++ tests/test_advisor.py
@@ -0,0 +1,9 @@
+"""
+Module to contain all tests pertaining to agent advisors. 
+"""
+
+from respons_ml import PromClient
+from utilities  import prom_cpu_mem_queries
+
+def test_prometheus_advisor():
+    

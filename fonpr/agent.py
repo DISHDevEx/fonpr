9da597b038178f709639e93a9a186fa448d494ad
matthew.reed <matthew.reed@dish.com>
2023-04-12 21:17:10 +0000

    Updated agent script flow to execute advisor, process advisor output into value update requests, and then update remote repository using action handler. Moved previous looping script into its own function that can be called on its own.

--- fonpr/agent.py
+++ fonpr/agent.py
@@ -12,37 +12,7 @@ from action_handler import ActionHandler, get_token
 
 def print_lim_reqs():
     """
-    Create a prometheus client, connect to server, make queries, and print limits and requests for all pods.
-    V0 logic for the respons agent.
-    For V0 the agent will use the max/avg of CPU and Memory as the limits/requests.
-
-    Returns
-    -------
-        None. (logging and printing data for v0)
-    """
-    
-    dict_lim_req = collect_lim_reqs()
-            
-    #Transcribe the dictionary to dataframe. 
-    df_lim_req = pd.DataFrame.from_dict(
-        dict_lim_req,
-        orient="index",
-        columns=[
-            "lim_cpu_cores",
-            "req_cpu_cores",
-            "lim_memory_bytes",
-            "req_memory_bytes",
-        ],
-    )
-
-    logging.info("\t" + df_lim_req.to_string().replace("\n", "\n\t"))
-    print(df_lim_req.head(100))
-    
-def collect_lim_reqs(prom_endpoint='http://10.0.101.236:9090') -> dict:
-    """
-    Create a prometheus client, connect to server, make queries, and print limits and requests for all pods.
-    V0 logic for the respons agent. 
-    For V0 the agent will use the max/avg of CPU and Memory as the limits/requests. 
+    Prints the output of collect_lim_reqs to logs and stdout as a dataframe. 
 
     Returns
     -------
@@ -119,12 +89,15 @@ def collect_lim_reqs() -> dict:
             print("empty records found, ignoring")
             
     return dict_lim_req
-    
 
-if __name__ == "__main__":
+def loop_queries() -> None:
     """
-    Call the print_lim_reqs() function for a predefined set of iterations, with a waiting period.
+    Call the print_lim_reqs() function for a predefined set of iterations, with a waiting period. 
     10000 iterations, and wait period of 5 seconds.
+    
+    Returns
+    -------
+        None.
     """
 
     num_iterations = 10000

708693c55bf40e5682f14505bf2d8a09017e8baa
vinnysharm <vinayak.sharma@dish.com>
2023-04-17 17:00:53 +0000

    making test non trivial, and adding formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
 """
-Define fixtures and configuration
-that can be reused throughout pytest without redefinition.
-It also defines the configurations for pytest.
+Define fixtures and configurations that can be reused is all pytests.
 """
 
 import pytest
@@ -10,7 +8,7 @@ import pytest
 @pytest.fixture(scope="module")
 def prom_memory_query():
     """
-    string written in promql to retrieve memory on a per pod basis
+    String written in promql to retrieve memory on a per pod basis.
     """
     max_memory_query = "sum by (pod)  (max_over_time(container_memory_usage_bytes[3h]))"
     return max_memory_query
@@ -19,11 +17,7 @@ def prom_memory_query():
 @pytest.fixture(scope="module")
 def sample_response():
     """
-    This is a sample respons that prometheus server would return. 
-    List of nested dictionaries is the format that the prometheus server returns. 
-    
-    In this sample respons, there is only one pod running. 
-    Hence we get a list containing one element. This one element is a nested dictionary. 
+    Return a sample of nested dictionaries in the same format that the Prometheus server returns.
     """
     sample_response = [{
         "metric": {"pod": "open5gs-smf-684588d586-gbqss"},

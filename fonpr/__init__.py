7aa97a774f3d12b8ef94a67dde9ad95fe0c69aad
vinayaksharm <vinayak.sharma@dish.com>
2023-05-11 23:43:38 +0000

    finishing up prom query for bbo agent

--- fonpr/__init__.py
+++ fonpr/__init__.py
@@ -3,7 +3,9 @@ FONPR
 A package to react in real time to EKS cluster behavior 
 to fine tune helm charts for deployed applications. 
 """
-from .utilities import prom_cpu_mem_queries,ec2_cost_calculator
+from .utilities import prom_cpu_mem_queries
+from .utilities import ec2_cost_calculator
+from .utilities import prom_network_upf_query
 
 from .advisors import PromClient
 
--- fonpr/bbo_agent.py
+++ fonpr/bbo_agent.py
@@ -5,7 +5,7 @@ import time
 import logging
 from collections import defaultdict
 from advisors import PromClient
-from utilities import prom_cpu_mem_queries,ec2_cost_calculator
+from utilities import prom_network_upf_query,ec2_cost_calculator
 from action_handler import ActionHandler, get_token
 import argparse
 import logging
@@ -19,17 +19,19 @@ def reward_function(throughput,infra_cost):
 
     cost_conversion_coefficient = 3.33/1000000000
     reward = (throughput)*(cost_conversion_coefficient) - infra_cost
-    
+    return reward
 
 def get_throughput():
-    
-    throughput = 1000000000
-    
-    return throughput
+    prom_client_advisor = PromClient('http://10.0.104.52:9090')
+    prom_client_advisor.set_queries_by_function(prom_network_upf_query)
+    avg_upf_network = prom_client_advisor.run_queries()
+    return avg_upf_network
 
 def get_infra_cost(size="Large"):
     if(size == "Small"):
-        return ec2_cost_calculator()
+        return ec2_cost_calculator("t3.medium")
+    if(size == "Large"):
+        return ec2_cost_calculator("m4.large")
     
 
 
@@ -67,34 +69,42 @@ if __name__ == "__main__":
     Test cycle: running execute_agent_cycle repeatedly to demonstrate base
     functionality in the kubernetes environment.
     """
-    
-    ##set a sizing variable for reward function to intake
-    current_sizing_selected = 'Large'
-    
-    ##instantiate some logging
-    logging.basicConfig(level=logging.INFO)
-    logging.info('Launching FONPR Agent')
-    
-    #setup google vizier for BBO 
-    study_config = vz.StudyConfig(algorithm='GAUSSIAN_PROCESS_BANDIT')
-    study_config.search_space.root.add_categorical_param('size', ['Small', 'Large'])
-    study_config.metric_information.append(vz.MetricInformation('metric_name', goal=vz.ObjectiveMetricGoal.MAXIMIZE))
-    study = clients.Study.from_study_config(study_config, owner='vinayak', study_id='example1')
-    
-    ##Run BBO
-    for i in range(1):
-        # logging.info('Executing update cycle.')
-        
-        suggestions = study.suggest(count=1)
+    observed_throughput = get_throughput()
+    print(observed_throughput)
 
-        for suggestion in suggestions:
-            params = suggestion.parameters
-            update_yml(size = params['size'] )
-            ##sleep for 60 seconds, to see the impact of changing sizing
-            time.sleep(60)
-            observed_throughput = get_throughput()
+    # ##instantiate some logging
+    # logging.basicConfig(level=logging.INFO)
+    # logging.info('Launching FONPR Agent')
+    
+    # #setup google vizier for BBO 
+    # study_config = vz.StudyConfig(algorithm='GAUSSIAN_PROCESS_BANDIT')
+    # study_config.search_space.root.add_categorical_param('size', ['Small', 'Large'])
+    # study_config.metric_information.append(vz.MetricInformation('reward', goal=vz.ObjectiveMetricGoal.MAXIMIZE))
+    # study = clients.Study.from_study_config(study_config, owner='vinayak', study_id='example1')
+    
+    # ##Run BBO
+    # for i in range(1):
+        
+    #     logging.info('Executing update cycle.')
+        
+    #     suggestions = study.suggest(count=1)
+
+    #     for suggestion in suggestions:
+    #         params = suggestion.parameters
+    #         # update_yml(size = params['size'] )
             
-            reward = reward_function(observed_throughput,)
+    #         ##sleep for 60 seconds, to see the impact of changing sizing
+    #         time.sleep(1)
+            
+    #         #get the observations for the system to build out reward function
+    #         #feed reward to BBO algorithm, and complete the trial
+    #         observed_throughput = get_throughput()
+    #         observed_cost = get_infra_cost(params['size'] )
+    #         reward = reward_function(observed_throughput,observed_cost)
+    #         print(observed_throughput,observed_cost)
+    #         suggestion.complete(vz.Measurement({'reward': reward}))
+            
+            
             
         
 

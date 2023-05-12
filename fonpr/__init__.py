c464b1f759d68ab15e4ea2a834ed94417a685aa9
Matthew Reed <matthew.reed@dish.com>
2023-05-12 23:15:17 +0000

    Added skeleton for environment in fonpr_env.py

--- fonpr/__init__.py
+++ fonpr/__init__.py
@@ -15,3 +15,4 @@ from .action_handler import ActionHandler, get_token
 from .rl_infrastructure import FonprDqn
 from .rl_infrastructure import ReplayBuffer
 from .rl_infrastructure import Driver
+from .agents import FONPR_Env

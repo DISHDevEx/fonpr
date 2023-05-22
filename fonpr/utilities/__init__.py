548d9f57e0bcee91139b7670aa8637afaad158cd
Matthew Reed <matthew.reed@dish.com>
2023-05-22 17:15:52 +0000

    Adapting obs and reward for lack of access to real time instance type data.

--- fonpr/utilities/__init__.py
+++ fonpr/utilities/__init__.py
@@ -3,6 +3,7 @@ Utilites
 Aids the response ML framework to ingest, process, and output
 """
 from .prom_queries import prom_cpu_mem_queries
+from .prom_queries import prom_query_rl_upf_experiment1
 from .prom_queries import prom_network_upf_query
 from .prom_queries import prom_network_upf_interfaces_query
 from .cost_function import ec2_cost_calculator

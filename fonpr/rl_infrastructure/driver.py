7f7250a5700ba94211c27e9af7185d8a345d76cf
vinayaksharm <vinayak.sharma@dish.com>
2023-05-22 20:04:32 +0000

    continuing modularization of dqn agent

---
+++ fonpr/rl_infrastructure/driver.py
@@ -0,0 +1,161 @@
+import time
+import logging
+from advisors import PromClient
+from utilities import prom_network_upf_query, ec2_cost_calculator
+from action_handler import ActionHandler, get_token
+import tensorflow as tf
+import numpy as np
+import tf_agents
+from tf_agents.trajectories import trajectory
+
+class Driver:
+    
+    def __init__(self,prom_endpoint = "http://10.0.104.52:9090",wait_period = 1 ):
+        self.prom_endpoint = prom_endpoint
+        self.wait_period = wait_period
+    
+    def reward_function(self, throughput, infra_cost):
+        """
+        Calculates the reward for the agent to receive based off of throughput and infrastructure cost.
+    
+        Parameters
+        ----------
+            throughput : float
+                The average network transmitted for the last hour from UPF pod.
+            infra_cost : float
+                The cost of running the ec2 sizing for the UPF pod.
+    
+        Returns
+        -------
+            avg_upf_network: float
+                The average network transmitted for the last hour from UPF pod.
+        """
+    
+        # All cost is calculated on an hourly basis
+        # Throughput must be in bytes.
+        # The cost conversion coefficient converts 1 gigabyte to 3.33$(https://newsdirect.com/news/mobile-phone-data-costs-7x-more-in-the-us-than-the-uk-158885004?category=Communications).
+        cost_conversion_coefficient = 3.33 / (10**9)
+        reward = (throughput) * (cost_conversion_coefficient) - infra_cost
+        return reward
+    
+    
+    def get_throughput(self):
+        """
+        Calculates the average network bytes transmitted from the UPF pod for the last hour.
+    
+        Returns
+        -------
+            avg_upf_network: float
+                The average network transmitted for the last hour from UPF pod.
+        """
+    
+        # Set the prometheus client endpoint for the prometheus server in Respons-Nuances.
+        prom_client_advisor = PromClient(self.prom_endpoint)
+        prom_client_advisor.set_queries_by_function(prom_network_upf_query)
+        avg_upf_network = prom_client_advisor.run_queries()
+        avg_upf_network = float(avg_upf_network[0][0]["value"][1])
+        return avg_upf_network
+    
+    
+    def get_infra_cost(self,size="Large"):
+        """
+        Calculates the hourly cost of the infrastructure based off the categorical value of size.
+    
+        Parameters
+        ----------
+            size : str
+                Specify the nodegroup sizing for upf.
+        Returns
+        -------
+            cost: int
+                The hourly cost of running the ec2 sizing for the UPF pod.
+        """
+    
+        if size == "Small":
+            return ec2_cost_calculator("t3.medium")
+        if size == "Large":
+            return ec2_cost_calculator("m4.large")
+    
+    
+    def update_yml(self,
+        size="Large",
+        gh_url="https://github.com/DISHDevEx/napp/blob/aakash/hpa-nodegroups/napp/open5gs_values/5gSA_no_ues_values_with_nodegroups.yaml",
+        dir_name="napp",
+    ) -> None:
+        """
+        Updates the controlling document in its remote repo using the action handler.
+    
+        Parameters
+        ----------
+            size : str
+                Specify the nodegroup sizing for upf.
+            gh_url : str
+                URL pointing to the target value.yaml file in GitHub (e.g. 'https://github.com/DISHDevEx/openverso-charts/blob/matt/gh_api_test/charts/respons/test.yaml')
+            dir_name : str
+                Name of first directory in path to the yaml file (empty string if the file is at the root of the repo)
+        """
+    
+        # Requested actions is a dictionary specifying the pod to modify, and the sizing for that pod.
+        requested_actions = {"target_pod": "upf", "values": size}
+    
+        # Update remote repository with requested values.
+        hndl = ActionHandler(get_token(), gh_url, dir_name, requested_actions)
+        hndl.fetch_update_push_upf_sizing()
+        logging.info("Agent update complete!")
+        
+        
+        
+    def take_action_get_next_timestep(self,action_step):
+        
+        if(action_step.action ==0):
+            self.update_yml(size="Small")
+        
+        if(action_step.action ==1):
+            self.update_yml(size="Large")
+            
+        time.sleep(self.wait_period)
+        
+        throughput = self.get_throughput()
+        infra_cost = self.get_infra_cost()
+        reward = self.reward_function(throughput,infra_cost)
+    
+        discount  = tf.convert_to_tensor(np.array(1,np.float32))
+        observation = tf.convert_to_tensor(np.array([throughput],np.float32))
+        reward = tf.convert_to_tensor(np.array(reward,np.float32))
+        step_type = tf.convert_to_tensor(np.array(1,np.float32))
+        
+        next_time_step = tf_agents.trajectories.TimeStep(discount = discount,observation = observation, reward=reward, step_type = step_type )
+        return next_time_step
+     
+     
+     
+        
+    def drive(self,max_steps=10,policy=-1, observer=-1):
+        
+        throughput = self.get_throughput()
+        infra_cost = self.get_infra_cost()
+        reward = self.reward_function(throughput,infra_cost)
+        
+        discount  = tf.convert_to_tensor(np.array(1,np.float32))
+        observation = tf.convert_to_tensor(np.array([throughput],np.float32))
+        reward = tf.convert_to_tensor(np.array(reward,np.float32))
+        step_type = tf.convert_to_tensor(np.array(0,np.float32))
+        
+        current_timestep = tf_agents.trajectories.TimeStep(discount = discount,observation = observation, reward=reward, step_type = step_type )
+        
+    
+        for step in range(max_steps):
+            
+            if(step == 0):
+                policy_state = policy.get_initial_state(1)
+                
+            action_step = policy.action(current_timestep,policy_state)
+            next_time_step = self.take_action_get_next_timestep(action_step)
+            action_step_with_previous_state = action_step._replace(state=policy_state)
+            traj = trajectory.from_transition(current_timestep, action_step_with_previous_state, next_time_step)
+            observer(traj)
+            current_timestep = next_time_step
+            policy_state = action_step.state
+        
+        return current_timestep, policy_state
+            
--- fonpr/rl_infrastructure/replay_buffer.py
+++ fonpr/rl_infrastructure/replay_buffer.py
@@ -1,27 +1,35 @@
-import reverb
-
 """
 Create a replay buffer on a per agent basis. The replay buffer uses the agent 
 specifications 
 """
-
+from tf_agents.specs import tensor_spec
+from tf_agents.replay_buffers import reverb_replay_buffer
+from tf_agents.replay_buffers import reverb_utils
+import reverb
 
 class ReplayBuffer:
     
-    def __init__(self,agent):
+    def __init__(self,agent,replay_buffer_max_length = 100000,table_name = 'uniform_table'):
+        
         self.agent = agent
+        self.replay_buffer_max_length=replay_buffer_max_length
+        self.table_name = table_name
+        
+        self.replay_buffer_signature, self.table, self.reverb_server,self.replay_buffer,self.rb_observer = self.create_replay_buffer()
+        
         
     def create_replay_buffer(self):
-        
-        table_name = 'uniform_table'
+    
+
         replay_buffer_signature = tensor_spec.from_spec(
-          agent.collect_data_spec)
+          self.agent.collect_data_spec)
         replay_buffer_signature = tensor_spec.add_outer_dim(
         replay_buffer_signature)
         
+        
         table = reverb.Table(
-        table_name,
-        max_size=replay_buffer_max_length,
+        self.table_name,
+        max_size=self.replay_buffer_max_length,
         sampler=reverb.selectors.Uniform(),
         remover=reverb.selectors.Fifo(),
         rate_limiter=reverb.rate_limiters.MinSize(1),
@@ -30,12 +38,14 @@ class ReplayBuffer:
         reverb_server = reverb.Server([table])
         
         replay_buffer = reverb_replay_buffer.ReverbReplayBuffer(
-        agent.collect_data_spec,
-        table_name=table_name,
+        self.agent.collect_data_spec,
+        table_name=self.table_name,
         sequence_length=2,
         local_server=reverb_server)
         
         rb_observer = reverb_utils.ReverbAddTrajectoryObserver(
         replay_buffer.py_client,
-        table_name,
+        self.table_name,
         sequence_length=2)
+        
+        return replay_buffer_signature, table, reverb_server, rb_observer

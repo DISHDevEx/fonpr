242ff7e35d13d92f3f28d8b51424147725aadc1a
vinayaksharm <vinayak.sharma@dish.com>
2023-05-24 07:11:14 +0000

    finishing up docstrings for all of rl infra

--- fonpr/rl_infrastructure/replay_buffer.py
+++ fonpr/rl_infrastructure/replay_buffer.py
@@ -1,6 +1,6 @@
 """
 Create a replay buffer on a per agent basis. The replay buffer uses the agent 
-specifications 
+specifications.
 """
 from tf_agents.specs import tensor_spec
 from tf_agents.replay_buffers import reverb_replay_buffer
@@ -9,6 +9,52 @@ import reverb
 
 
 class ReplayBuffer:
+    """
+    ReplayBuffer helps to a reverb server to provide replay buffer to agent. This helps the agent train on random samples
+    Independantly and Identically Distributed (IID) from the replay buffer. 
+    
+    Attributes
+    ----------
+        agent: dqn_agent.DqnAgent()
+            The TensorFlow dqn agent. 
+            
+        replay_buffer_max_length: Int
+            The length of the replay buffer,before a fifo popping mechanism gets implemented.
+            
+        table_name: String
+            Name of the reverb server table to be implemented. 
+                
+        sequence_length = Int
+            Number of consecutive samples to bind together into a single sample for training.
+        
+        replay_buffer_signature: agent.collect_data_spec
+            Defines the structure or signature of the replay buffer.  
+            
+        table: reverb.Table
+            This is the underlying table that the replay buffer will interact with. 
+            
+        reverb_server: reverb.Server([table])
+            Turns the table into a server(allows api interactions)
+
+        replay_buffer: reverb_replay_buffer.ReverbReplayBuffer(reverb_server)
+            Turns the server into a replay buffer. 
+            
+        rb_observer: reverb_utils.ReverbAddTrajectoryObserver
+            Allows driver to use the replay buffer as an observer. rb_observer has the ability 
+            to collect experience. 
+
+    Methods
+    -------
+        _create_replay_buffer():
+            Private function autocalled during constructor __init__. 
+            Creates replay_buffer_signature,table,reverb_server,replay_buffer,rb_observer.
+
+        get_replay_buffer_as_dataset():
+            Returns replay buffer as dataset. 
+        
+        get_replay_buffer_as_iterator():
+            Returns a replay buffer as dataset, with an iterator wrapper. 
+    """
     def __init__(
         self,
         agent,
@@ -26,9 +72,30 @@ class ReplayBuffer:
             self.reverb_server,
             self.replay_buffer,
             self.rb_observer,
-        ) = self.create_replay_buffer()
+        ) = self._create_replay_buffer()
 
-    def create_replay_buffer(self):
+    def _create_replay_buffer(self):
+        """
+        This function is called as part of the constructor. 
+        Creates all of the components of the replay buffer. 
+        Returns
+        ---------
+        replay_buffer_signature: agent.collect_data_spec
+            Defines the structure or signature of the replay buffer.  
+            
+        table: reverb.Table
+            This is the underlying table that the replay buffer will interact with. 
+            
+        reverb_server: reverb.Server([table])
+            Turns the table into a server(allows api interactions)
+
+        replay_buffer: reverb_replay_buffer.ReverbReplayBuffer(reverb_server)
+            Turns the server into a replay buffer. 
+            
+        rb_observer: reverb_utils.ReverbAddTrajectoryObserver
+            Allows driver to use the replay buffer as an observer. rb_observer has the ability 
+            to collect experience. 
+        """
         replay_buffer_signature = tensor_spec.from_spec(self.agent.collect_data_spec)
         replay_buffer_signature = tensor_spec.add_outer_dim(replay_buffer_signature)
 
@@ -59,6 +126,13 @@ class ReplayBuffer:
         return replay_buffer_signature, table, reverb_server, replay_buffer, rb_observer
 
     def get_replay_buffer_as_dataset(self, num_parallel_calls=3, batch_size=10):
+        """
+        Returns replay buffer as dataset. 
+        
+        Returns
+        ---------
+        dataset: replay_buffer.as_dataset()
+        """
         dataset = self.replay_buffer.as_dataset(
             num_parallel_calls=num_parallel_calls,
             sample_batch_size=batch_size,
@@ -68,6 +142,13 @@ class ReplayBuffer:
         return dataset
 
     def get_replay_buffer_as_iterator(self, num_parallel_calls=3, batch_size=10):
+        """
+        Returns replay buffer as dataset with an iterator wrapper. 
+        
+        Returns
+        ---------
+        iterator: iter(replay_buffer.as_dataset())
+        """
         dataset = self.get_replay_buffer_as_dataset(num_parallel_calls, batch_size)
         iterator = iter(dataset)
         return iterator

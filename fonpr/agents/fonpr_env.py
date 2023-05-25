72410a6c40a8cf050445c9c8073d6826dc144f92
Matthew Reed <matthew.reed@dish.com>
2023-05-25 17:38:07 +0000

    Initial query structure for mapping host_ip to instance_type added

--- fonpr/agents/fonpr_env.py
+++ fonpr/agents/fonpr_env.py
@@ -52,25 +52,38 @@ class FONPR_Env(Env):
         prom_client_advisor.set_queries_by_function(prom_query_rl_upf_experiment1())
         prom_response = prom_client_advisor.run_queries()
         
-        df = pd.DataFrame(prom_response[0]['values']) # Create dataframe on throughput values
-        df = df.set_index(0) # Use timestamps for index
+        df = pd.DataFrame(prom_response[0][0]['values'], columns=['DateTime', 'Throughput']) # Create dataframe on throughput values
+        df = df.set_index('DateTime') # Use timestamps for index
         df.index = pd.to_datetime(df.index, unit='s')
         
-        df[1] = df[1].astype(float)
-        df[1] = df[1] - df.iloc[0,0] # Normalize all throughput to value at first timestamp
+        df['Throughput'] = df['Throughput'].astype(float)
+        df['Throughput'] = df['Throughput'] - df.iloc[0,0] # Normalize all throughput to value at first timestamp
         df = df.interpolate() # Linear interpolation for any missing values
-        df = df.resample(f'{int(60/self.sample_rate)}s').mean()
+        df = df.resample(f'{int(60/self.sample_rate)}s').mean() # Keep input size consistent
         df = df.interpolate() # Linear interp again for any NaNs created by resample
         
-        through_vals = df[1].values # Get numpy array for processed throughput values
+        through_vals = df['Throughput'].values # Get numpy array for processed throughput values
         
         # Prepend zeroes if less samples present than required
         if len(through_vals) != self.samples:
             through_vals = np.insert(through_vals, 0, np.zeros(self.samples - len(through_vals)))
         
         # Process pod info
-        # TODO: for all pods found over timeframe, isolate host_ip and timestamps
-        # TODO: for all pods found over timeframe, map host_ip to instance-type
+        pods = {}
+        for i, pod in enumerate(prom_response[1]):
+            
+            # isolate host_ip and timestamps
+            host_ip = pod['metric']['host_ip']
+            values = pod['values']
+            
+            # map host_ip to instance-type
+            node_name = 'ip-' + host_ip.replace('.', '-') + '.ec2.internal'
+            prom_client_advisor.set_queries_by_list(['kube_node_labels{node=\'' + node_name + '\'}'])
+            node_labels = prom_client_advisor.run_queries()
+            instance_type = node_labels[0]['metric']['label_node_kubernetes_io_instance_type']
+            
+            pods[f'pod{i}'] = {'host_ip': host_ip, 'values': values, 'instance_type': instance_type}
+            
         # TODO: for each state variable ('Large instance On', 'Small instance On'), use instance-type and timesteps to map boolean
         # TODO: reshape to get vector of obs_space specified length
         # TODO: interpolate in case reshape creates nulls
@@ -91,7 +104,7 @@ class FONPR_Env(Env):
         return observation, info
 
     def step(self, action):
-        gh_url="https://github.com/DISHDevEx/napp/blob/aakash/hpa-nodegroups/napp/open5gs_values/5gSA_no_ues_values_with_nodegroups.yaml",
+        gh_url="https://github.com/DISHDevEx/napp/blob/matt/test_update/napp/open5gs_values/5gSA_no_ues_values_with_nodegroups.yaml"
         dir_name="napp"
         
         if action == 0: # No-Op; do nothing
@@ -99,10 +112,10 @@ class FONPR_Env(Env):
             pass
         elif action == 1: # Transition to Large instance
             hndl = ActionHandler(get_token(), gh_url, dir_name, {"target_pod": "upf", "values": "Large"})
-            hndl.fetch_update_push()
+            hndl.fetch_update_push_upf_sizing()
         elif action == 2: # Transition to Small instance
             hndl = ActionHandler(get_token(), gh_url, dir_name, {"target_pod": "upf", "values": "Small"})
-            hndl.fetch_update_push()
+            hndl.fetch_update_push_upf_sizing()
             
         # sleep(self.obs_period * 60) # Sleep for observation period before retrieving next observation
         sleep(10) # For testing

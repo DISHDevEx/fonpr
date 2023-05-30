a7868461853834e35e1674e87a747df334f0bb77
Matthew Reed <matthew.reed@dish.com>
2023-05-30 20:34:23 +0000

    Formatting update

--- fonpr/sac_agent.py
+++ fonpr/sac_agent.py
@@ -5,25 +5,28 @@ Script for running a Soft Actor Critic based agent in using the FONPR_Env Interf
 from interfaces import FONPR_Env
 from ray.tune.logger import pretty_print
 from ray.rllib.algorithms import sac
+import logging
 
 if __name__ == "__main__":
+    
+    logging.basicConfig(level=logging.INFO)
     
     env_config={'render_mode':None, 'window':15, 'sample_rate':4, 'obs_period':1}
     
     config = (
         sac.SACConfig()
         .environment(env=FONPR_Env, env_config=env_config)
-        .rollouts(num_rollout_workers=0)
+        .rollouts(num_rollout_workers=0) # Not parallelizable in Open5GS; using just the local worker
         .framework('tf2')
         .reporting(min_sample_timesteps_per_iteration=30)
         .training(train_batch_size=8, num_steps_sampled_before_learning_starts=16)
         )
-    config.replay_buffer_config.update(capacity=32)
+    config.replay_buffer_config.update(capacity=32) # Keeping small for initial experiments
     agent = config.build()
     
     counter = 0
     while True:
         counter += 1
-        print(f'---- Training cycle {counter} started ----')
+        logging.info(f'---- Training cycle {counter} started ----')
         agent.train()
-        print(f'---- Training cycle {counter} complete ----')
+        logging.info(f'---- Training cycle {counter} complete ----')
